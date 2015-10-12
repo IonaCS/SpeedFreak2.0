5a696836fd9d637173ec462db7adc04d9b8abbac
IonaCS <ionakat@gmail.com>
2015-10-13 10:48:47 +1300

    refactor car move method

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
 var car = document.getElementById("car-avatar");
 car.style.left = "0px";
 
-function moveLeft() {
-	car.style.left = parseInt(car.style.left) -5 + "px";
-}
-
-function moveRight() {
-	car.style.left = parseInt(car.style.left) +5 + "px";
+function move(leftOrRight) {
+	car.style.left = parseInt(car.style.left) + Math.sign(leftOrRight)*5 + "px";
 }
 
 document.body.onkeydown = function(e){
   key = e.keyCode || e.charCode|| e.which;
   if (key === 37) {
-  	moveLeft();
+  	move(-1);
   }
 	else if (key === 39) {
-		moveRight();
+		move(1);
 	}
 	else {
 		return;
