e9724b5f1d2712dadad4e74440547b90dcceb22d
IonaCS <ionakat@gmail.com>
2015-10-16 16:51:02 +1300

    get creepy men spawning too

--- js/app.js
+++ js/app.js
@@ -17,41 +17,43 @@ document.body.onkeydown = function(e) {
   key = e.keyCode || e.charCode|| e.which;
   if (key === 37 && parseInt(car.style.left) > 0) {
   	moveHorizontal(-1);
-  }
-  else if (key === 38 && parseInt(car.style.top) > -350) {
+  } else if (key === 38 && parseInt(car.style.top) > -350) {
 		moveVertical(-1);
-	}
-	else if (key === 39 && parseInt(car.style.left) < 440) {
+	}	else if (key === 39 && parseInt(car.style.left) < 440) {
 		moveHorizontal(1);
-	}
-	else if (key === 40 && parseInt(car.style.top) < 0) {
+	}	else if (key === 40 && parseInt(car.style.top) < 0) {
 		moveVertical(1);
-	}
-	else {
+	} else {
 		return;
 	}
 }
 
 function spawnObject() {
-	var cocktail = document.createElement("img");
-	cocktail.className = "cocktail object";
-	cocktail.src = "img/cocktail.png";
-	roads[0].appendChild(cocktail);
-	cocktail.style.left = Math.floor(Math.random() * 410) + "px";
-	cocktail.style.top = "0px"
+	var newObj = document.createElement("img");
+	var occuranceChance = Math.floor(Math.random() * 5);
+	if (occuranceChance >= 3) {
+		newObj.className = "cocktail object";
+		newObj.src = "img/cocktail.png";
+	} else {
+		newObj.className = "flasher object";
+		newObj.src = "img/flasher.png";
+	}
+	newObj.style.left = Math.floor(Math.random() * 410) + "px";
+	newObj.style.top = "0px";
+	roads[0].appendChild(newObj);
 }
 
 function moveObjectDown() {
 	for (var i=0; i<objects.length; i++) {
 		objects[i].style.top = parseInt(objects[i].style.top) + 5 + "px";
-		if (parseInt(objects[i].style.top) > 435) {
-			roads[0].removeChild(objects[i])
+		if (parseInt(objects[i].style.top) > 415) {
+			roads[0].removeChild(objects[i]);
 		}
 	}
 }
 
 function timer() {
   setInterval("moveObjectDown()", 100);
-  setInterval("spawnObject()", Math.floor(Math.random() * 3500) + 1000)
+  setInterval("spawnObject()", Math.floor(Math.random() * 3500) + 1000);
 }
 timer();
